60a7d77c293f578e6dc5491bd08a931b9bbed23e
yx_landongjie <ldj2472599@163.com>
2019-09-29 11:44:55 +0800

    增加SayHi例子

---
+++ src/components/SayHi.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='hi'>
+    Hi Vue!
+    <h1>
+      {{message}}
+    </h1>
+    <button @click="show_my_value()">显示初始值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: '你好Vue，我是message的初始值'
+    }
+  },
+  methods: {
+    show_my_value: function () {
+      alert('my_value: ' + this.message)
+    }
+  }
+}
+</script>
+
+<style>
+.hi {
+  color: red;
+  font-size: 20px;
+}
+</style>
